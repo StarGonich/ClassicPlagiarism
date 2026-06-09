f9916508a5a850ff1905d08f54d91594d651b468
StarGonich <alexey8837@gmail.com>
2026-06-09 18:01:55 +0300

    Берутся только поддерживыемые Joern языки

--- PreDelete.py
+++ PreDelete.py
@@ -9,13 +9,13 @@ from PlagiarismDB import PlagiarismDB
 # LOGPATH = "log.txt"
 # ARCHIVEPATH = "602776"
 
-PLATFORM = "Yandex"
-LOGPATH = "log-911-1.xml"
-ARCHIVEPATH = "submits-911-1"
-
 # PLATFORM = "Yandex"
-# LOGPATH = "log-911-2.xml"
-# ARCHIVEPATH = "submits-911-2"
+# LOGPATH = "log-911-1.xml"
+# ARCHIVEPATH = "submits-911-1"
+
+PLATFORM = "Yandex"
+LOGPATH = "log-911-2.xml"
+ARCHIVEPATH = "submits-911-2"
 
 ARCHIVEPATH_OK = ARCHIVEPATH + '_OK'
 
@@ -45,7 +45,7 @@ if PLATFORM == "Yandex":
                         SELECT language FROM submissions WHERE submission_code = ? AND contest_id = ?
                         """, (submission_code, contest_id))
                 language = cursor.fetchone()[0]
-                if language in ('.txt', '.pas'):
+                if language not in ('.py', '.c', '.cpp', '.java', '.js', '.cs', '.go', '.rs', '.php'):
                     continue
                 count += 1
                 fullfilename = filename + language
@@ -77,37 +77,4 @@ elif PLATFORM == "Codeforces":
             target_file = os.path.join(ARCHIVEPATH_OK, fullfilename)
             if os.path.exists(source_file):
                 shutil.copy2(source_file, target_file)
-
-
-
-
-
-
-# submissions = 
-# print(f"Всего {len([sub for sub in logparser.submissions])}")
-# # Создать папку 602776_OK
-# for sub in logparser.submissions:
-#     filename, ext = os.path.splitext(sub.filename)
-#     # print(ext)
-#     if sub.verdict == 'OK':
-#         count += 1
-#         # Обрабатываем расширения файлов
-#         if ext in ('.pypy3-64', '.py3', '.pypy3'):
-#             fullfilename = filename + '.py'
-#             source_file = os.path.join(ARCHIVEPATH, sub.filename)
-#             target_file = os.path.join(ARCHIVEPATH_OK, fullfilename)
-
-#             # Переименовываем и копируем в новую папку
-#             if os.path.exists(source_file):
-#                 os.rename(source_file, os.path.join(ARCHIVEPATH, fullfilename))
-#                 shutil.copy2(os.path.join(ARCHIVEPATH, fullfilename), target_file)
-#         else:
-#             fullfilename = filename + ext
-#             source_file = os.path.join(ARCHIVEPATH, sub.filename)
-#             target_file = os.path.join(ARCHIVEPATH_OK, fullfilename)
-
-#             # Копируем файл в новую папку
-#             if os.path.exists(source_file):
-#                 shutil.copy2(source_file, target_file)
-
 print(f"Всего {count}")
